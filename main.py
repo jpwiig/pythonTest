613abe52200e43c5b53b7887a179d85b8a9fa112
jpwiig <jpwiig@gmail.com>
2022-03-03 21:10:40 +0100

    init

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ import pandas as pd
 
 df = pd.read_csv('rå/athlete_events.csv')
 listetest = ['b', [1, 2, 3, 4, 5]]
+
+sortert=df.stort_values("NOC")
 for i in listetest:
     print(df.to_string())
+
+for i in listetest:
+    print(sortert.to_string())
